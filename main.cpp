7e344d42faf516e2867a80fd775e4eff55f74be4
Natalia Vera <83522639+minoriflame@users.noreply.github.com>
2023-12-09 18:47:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,54 @@
-#include <cstddef>
-#include <fstream>
-#include <iostream>
-#include <omp.h>
-#include <vector>
+#include <cstddef> //para tamaños size_t
+#include <fstream> //manipulacion de archivos
+#include <iostream> // E/S estandar
+#include <omp.h> //OpenMP
+#include <vector> //vectores
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+//activar implementación de libreria stb
+#define STB_IMAGE_WRITE_IMPLEMENTATION  
+#include "stb_image_write.h" //Libreria externa para escribir imagenes
 
 using namespace std;
 
+// Dimensiones de la imagen
 #define HEIGHT 7121
 #define WIDTH 10681
 
-vector<size_t> parse_file(string path) {
-#pragma omp critical
+vector<size_t> parse_file(string path) { // toma la ruta de un archivo y devuelve vector size_t
+#pragma omp critical 
   cout << "[INFO] Poblando el archivo " << path << std::endl;
-  vector<size_t> aux(WIDTH * HEIGHT, 0);
+  vector<size_t> aux(WIDTH * HEIGHT, 0); //inicializa vector "aux" de tamaño widthxheight en 0
 
-  ifstream file(path);
+  ifstream file(path); //abre archivo ubicado en path
 
-  for (int i = 0; i < HEIGHT * WIDTH; ++i) {
+  for (int i = 0; i < HEIGHT * WIDTH; ++i) { //Itera sobre todos los elementos
     string out;
-    file >> out;
+    file >> out; //Lee un valor del archivo como una cadena
 
-    aux[i] = !out.compare("*") ? 300 : std::stoi(out);
+    aux[i] = !out.compare("*") ? 300 : std::stoi(out); //Asigna 300 si el valor es "*", sino convierte la cadena a entero y lo asigna
   }
 
-  file.close();
+  file.close(); //Cierra el archivo despues de leer todos los datos
 
-  return aux;
+  return aux; //Devuelve el vector aux con los datos leidos
 }
 
+//Definie funcion "pixeling" que toma un puntero a un arreglo de pixeles, ruta de archivo, vector "mean", valor "step" y un porcentaje
 void pixeling(uint32_t *pixels[], string path, vector<size_t> &mean, uint8_t step, float percentage) {
-  ifstream file(path);
+  ifstream file(path); //Abre el archivo ubicado en "path" para lectura
 
-  for (int i = 0; i < HEIGHT * WIDTH; ++i) {
+  for (int i = 0; i < HEIGHT * WIDTH; ++i) { //Itera sobre todos los elementos
     string out;
-    file >> out;
+    file >> out;  //Lee el archivo como una cadena
 
-    uint32_t value = !out.compare("*") ? mean[i] * percentage : (uint32_t)std::stoi(out);
+    uint32_t value = !out.compare("*") ? mean[i] * percentage : (uint32_t)std::stoi(out); //Calcula el valor del pix
     (*pixels)[i] = (value << step | (*pixels)[i]);
   }
 
   file.close();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {  //Verigica que se proporcionen al menos 6 argumentos
   if (argc < 6) {
     cout << "Error, falta la ruta de alguno de los .txt" << endl
          << "uso: main <alfa.txt> <rojo.txt> <verde.txt> <azul.txt> "
@@ -61,11 +64,12 @@ int main(int argc, char *argv[]) {
     pixels[i] = 0;
   }
 
-  vector<size_t> mean = parse_file(argv[5]);
+  vector<size_t> mean = parse_file(argv[5]);  //Lee el archivo "promedio.txt" y almacena los datos en el vector "mean"
 
   cout << "[INFO] Generando Imagen" << endl;
+  
   // Esto hace que cada función se ejecute en su propio hilo.
-#pragma omp parallel
+#pragma omp parallel 
 #pragma omp single
   {
 #pragma omp task
@@ -78,17 +82,17 @@ int main(int argc, char *argv[]) {
      pixeling(&pixels, argv[4], mean, 16, 0.11f);
   }
 
-  // Despejo memoria
+  // Despejo de memoria del vector mean
   mean = vector<size_t>();
 
   cout << "[INFO] Escribiendo imagen" << endl;
 
-  const char *file_path = "output.png";
+  const char *file_path = "output.png";  //Define la ruta del archivo de salida
   if (!stbi_write_png(file_path, WIDTH, HEIGHT, 4, pixels,
-                      sizeof(uint32_t) * WIDTH)) {
+                      sizeof(uint32_t) * WIDTH)) {  //Escribe la imagen en formato "PNG" usando libreria "STB"
     fprintf(stderr, "ERROR: could not write %s\n", file_path);
     return 1;
   }
 
-  return 0;
+  return 0;  //se ejecutó correctamente
 }
